a9b9f533f3bff01c342e015c81c76c66c58d7cca
lova233 <andrealovati.cube3@gmail.com>
2019-03-26 17:50:07 +0000

    determinate start button

--- js/game/states/Menu.js
+++ js/game/states/Menu.js
@@ -1,12 +1,19 @@
 PizzaConnection.Menu = {
         preload: function() {
+        
+
+        this.menuAction = new MenuActions(game)
+
         this.background =  game.add.sprite( 0, 0 , 'm_background');
         this.background.scale.setTo(0.6);
-        this.start = game.add.sprite(game.world.centerX,game.world.centerY + 50, 'm_start')
-        this.start.scale.setTo(0.7)
-        this.start.anchor.setTo(0.5)
-        this.start.alpha = 0;
-        game.add.tween(this.start).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true);        
+
+        
+        //     this.start
+        // this.start.button.scale.setTo(0.7)
+        // this.start.button.anchor.setTo(0.5)
+
+
+
         this.title = game.add.sprite(game.world.centerX,game.world.centerY - 100,'m_title')
         this.title.scale.setTo(0.5)
         this.title.anchor.setTo(0.5)
@@ -15,6 +22,12 @@ PizzaConnection.Menu = {
         this.pizzaBack.anchor.setTo(0.5)
         this.pizzaBack.scale.setTo(0.5)
         game.add.tween(this.pizzaBack).from( { x: -200 }, 2000, Phaser.Easing.Bounce.Out, true);
+        this.start = new Command (game,
+            this.menuAction,
+            Phaser.Keyboard.ENTER,
+            this.menuAction.get().START_GAME).createButton(game.world.centerX,game.world.centerY + 50, 358,133, 'm_start');
+            // this.start.button.alpha = 0;
+            // game.add.tween(this.start.button).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true);
     },
     create: function() {
         // this.preloadBar.cropEnabled = false;
