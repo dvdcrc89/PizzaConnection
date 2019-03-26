b31b54c9bfbd4d08c335079bd85195e8172f02e8
lova233 <andrealovati.cube3@gmail.com>
2019-03-26 17:49:36 +0000

    creates Start game action

---
+++ js/game/prefabs/MenuActions.js
@@ -0,0 +1,23 @@
+MenuActions = function(game) {
+    this.game = game;
+
+    const actions = {
+        START_GAME: "[MENU] START GAME",
+    }
+
+
+    this.get = function() {
+        return actions;
+    }
+
+    this.dispatch = function(action) {
+        switch (action) {
+            case actions.START_GAME:
+                game.state.start('Game');
+                break;
+
+        }
+    }
+
+
+}
